736d18fc9abeb7728e8f45c3b39ad7fd5e8e6ff3
Vincent Bauchart <vincent.bauchart@ingenico.com>
2023-04-06 20:26:31 +0000

    add a condition

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -3,6 +3,7 @@
 # Install nginx and its configuration
 #
 class nginx (
+  Boolean $install_default_page = true,
   Integer $port = 80,
 ) {
 
@@ -13,7 +14,22 @@ class nginx (
   file { '/etc/nginx/sites-enabled/default':
     content => template('nginx/default.conf.erb'),
     mode    => '0644',
-    owner   => root,
+    owner   => 'root',
+    require => Package['nginx'],
   }
 
+  if $install_default_page {
+
+    file { '/var/www/html/demo/index.html':
+      content => template('nginx/index.html.erb'),
+      mode    => '0644',
+      owner   => 'www-data',
+    }
+
+    file { '/var/www/html/demo/':
+      ensure => directory,
+      mode   => '0750',
+      owner  => 'www-data',
+    }
+  }
 }
